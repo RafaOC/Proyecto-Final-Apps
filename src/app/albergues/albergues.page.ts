5422e39697cb0aa0a37a7667ccf2c282079c468c
Euris Alexander Aquino <75188409+EurisAquino@users.noreply.github.com>
2022-12-10 15:14:54 -0400

    Albergues
    Se agregaron los valores de los albergues

--- src/app/albergues/albergues.page.ts
+++ src/app/albergues/albergues.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-albergues',
@@ -7,7 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlberguesPage implements OnInit {
 
-  constructor() { }
+  public lista:any;
+
+  constructor(private http: HttpClient) {
+    this.cargar();
+   }
+
+  cargar(){
+    this.http
+    .get<any>(
+      'https://adamix.net/defensa_civil/def/albergues.php'
+    )
+    .subscribe((res)=>{
+      console.log(res);
+      this.lista = res.datos;
+
+      for(var i = 0; i < this.lista.length; i++){
+        if(this.lista.capacidad == ""){
+          this.lista.capacidad = "Cantidad desconocida";
+        }
+      }
+    });
+  }
 
   ngOnInit() {
   }
