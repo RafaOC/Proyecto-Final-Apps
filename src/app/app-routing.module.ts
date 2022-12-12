7eee5502d045fed1a5f83bc29968f37ad325def7
reylishernandez29 <reylisfrancisco@outlook.es>
2022-12-11 21:06:15 -0400

    login fron vol2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -58,6 +58,18 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'reportar-situacion',
+    loadChildren: () => import('./reportar-situacion/reportar-situacion.module').then( m => m.ReportarSituacionPageModule)
+  },
+  {
+    path: 'mis-situaciones',
+    loadChildren: () => import('./mis-situaciones/mis-situaciones.module').then( m => m.MisSituacionesPageModule)
+  },
+  {
+    path: 'mapa-situaciones',
+    loadChildren: () => import('./mapa-situaciones/mapa-situaciones.module').then( m => m.MapaSituacionesPageModule)
+  },
 
 
 ];
---
+++ src/app/reportar-situacion/reportar-situacion.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reportar-situacion',
+  templateUrl: './reportar-situacion.page.html',
+  styleUrls: ['./reportar-situacion.page.scss'],
+})
+export class ReportarSituacionPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
